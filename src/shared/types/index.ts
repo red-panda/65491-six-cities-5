b3114b67b7a5ad5f2541664e3adaa30994bdf788
Nikolai Prokopenko <nprokopenko@affmy.com>
2023-10-10 18:32:54 +0300

    ADD: CLI

---
+++ src/shared/types/index.ts
@@ -0,0 +1,4 @@
+export * from './offer-type.enum.js';
+export * from './offer.type.js';
+export * from './user.type.js';
+export * from './convenience-type.enums.js'
---
+++ src/shared/types/offer-type.enum.ts
@@ -0,0 +1,6 @@
+export enum OfferType {
+  apartment = 'apartment',
+  house = 'house',
+  room = 'room',
+  hotel = 'hotel',
+}
