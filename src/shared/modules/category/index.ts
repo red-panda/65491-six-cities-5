2d3c632828f7043ace0743af76e49d22051a98df
Nikolai Prokopenko <nprokopenko@affmy.com>
2023-11-04 18:53:13 +0300

    ADD: Spec

---
+++ src/shared/modules/category/index.ts
@@ -0,0 +1,6 @@
+export * from './dto/create-category.dto.js';
+export * from './category-service.interface.js';
+export * from './category.entity.js';
+export * from './category.service.js';
+export * from './category.container.js';
+export * from './category.constant.js';
