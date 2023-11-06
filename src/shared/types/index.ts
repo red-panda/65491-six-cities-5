be99bd9d3baf743813fd23047dd92618a25d8b76
Nikolai Prokopenko <nprokopenko@affmy.com>
2023-11-06 13:14:23 +0300

    ADD: Express

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,7 +1,9 @@
 export * from './offer-type.enum.js';
 export * from './offer.type.js';
 export * from './user.type.js';
+export * from './category.type.js';
 export * from './convenience-type.enums.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './sort-type.enum.js';
+
